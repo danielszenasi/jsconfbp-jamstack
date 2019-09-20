67da9d23f272a3f4841c25b30a846bef8db1c90a
Daniel Szenasi <daniel.szenasi@supercharge.io>
2019-09-20 22:32:07 +0200

    feat: add style

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,42 +2,44 @@ import React from 'react';
 import { Link, StaticQuery, graphql } from 'gatsby';
 
 const IndexPage = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        allMarkdownRemark {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
+  <div className="p-24 flex flex-wrap items-center justify-center">
+    <StaticQuery
+      query={graphql`
+        query {
+          allMarkdownRemark {
+            edges {
+              node {
+                fields {
+                  slug
+                }
+                frontmatter {
+                  title
+                }
               }
             }
           }
         }
-      }
-    `}
-    render={data => {
-      return data.allMarkdownRemark.edges.map(
-        ({
-          node: {
-            fields: { slug },
-            frontmatter: { title }
-          }
-        }) => (
-          <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">
-                <Link to={slug}>{title}</Link>
+      `}
+      render={data => {
+        return data.allMarkdownRemark.edges.map(
+          ({
+            node: {
+              fields: { slug },
+              frontmatter: { title }
+            }
+          }) => (
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">
+                  <Link to={slug}>{title}</Link>
+                </div>
               </div>
             </div>
-          </div>
-        )
-      );
-    }}
-  ></StaticQuery>
+          )
+        );
+      }}
+    ></StaticQuery>
+  </div>
 );
 
 export default IndexPage;
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,58 +1,93 @@
 import React from 'react';
 import { StaticQuery, Link } from 'gatsby';
 
+const backgroundColors = ['purple', 'orange', 'teal'];
+
 const ProductsPage = () => (
-  <div id="app" class="antialiased text-gray-900">
-    <div class="bg-gray-200 min-h-screen p-8 flex flex-col justify-center">
-      <StaticQuery
-        query={graphql`
-          query {
-            allMarkdownRemark(filter: { frontmatter: { templateKey: { eq: "product-page" } } }) {
-              edges {
-                node {
-                  fields {
-                    slug
-                  }
-                  frontmatter {
-                    image
-                    price
-                    name
-                  }
+  <div className="p-24 flex flex-wrap items-center justify-center">
+    <StaticQuery
+      query={graphql`
+        query {
+          allMarkdownRemark(filter: { frontmatter: { templateKey: { eq: "product-page" } } }) {
+            edges {
+              node {
+                fields {
+                  slug
+                }
+                frontmatter {
+                  image
+                  price
+                  name
                 }
               }
             }
           }
-        `}
-        render={data => {
-          return data.allMarkdownRemark.edges.map(
-            ({
-              node: {
-                fields: { slug },
-                frontmatter: { name, image, price }
-              }
-            }) => (
-              <div>
-                <div className="relative pb-5/6">
-                  <img
-                    className="absolute h-full w-full object-cover rounded-lg shadow-md"
-                    src={image}
-                    alt="imageAlt"
-                  ></img>
+        }
+      `}
+      render={data => {
+        const color = backgroundColors[Math.floor(Math.random() * 3)];
+        return data.allMarkdownRemark.edges.map(
+          ({
+            node: {
+              fields: { slug },
+              frontmatter: { name, image, price }
+            }
+          }) => (
+            <Link to={slug}>
+              <div
+                className={`flex-shrink-0 m-6 relative overflow-hidden bg-${color}-500 rounded-lg max-w-xs shadow-lg`}
+              >
+                <svg
+                  className="absolute bottom-0 left-0 mb-8"
+                  viewBox="0 0 375 283"
+                  fill="none"
+                  style={{ transform: 'scale(1.5)', opacity: '0.1' }}
+                >
+                  <rect
+                    x="159.52"
+                    y="175"
+                    width="152"
+                    height="152"
+                    rx="8"
+                    transform="rotate(-45 159.52 175)"
+                    fill="white"
+                  />
+                  <rect
+                    y="107.48"
+                    width="152"
+                    height="152"
+                    rx="8"
+                    transform="rotate(-45 0 107.48)"
+                    fill="white"
+                  />
+                </svg>
+                <div className="relative pt-10 px-10 flex items-center justify-center">
+                  <div
+                    className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+                    style={{
+                      background: 'radial-gradient(black, transparent 60%)',
+                      transform: 'rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)',
+                      opacity: '0.2'
+                    }}
+                  ></div>
+                  <img className="relative w-40" src={image} alt=""></img>
                 </div>
-                <div className="relative px-4 -mt-16">
-                  <div className="bg-white p-6 rounded-lg shadow-lg">
-                    <h4 className="mt-1 font-semibold text-lg leading-tight truncate">
-                      <Link to={slug}>{name}</Link>
-                    </h4>
-                    <div className="mt-1">${price}</div>
+                <div className="relative text-white px-6 pb-6 mt-6">
+                  <div className="flex justify-between">
+                    <span className="block font-semibold text-xl">{name}</span>
+                    <span
+                      className={`block bg-white rounded-full text-${color}-500 text-xs font-bold px-3 py-2 leading-none flex items-center`}
+                    >
+                      ${price}
+                    </span>
                   </div>
                 </div>
               </div>
-            )
-          );
-        }}
-      ></StaticQuery>
-    </div>
+            </Link>
+          )
+        );
+      }}
+    ></StaticQuery>
   </div>
 );
 export default ProductsPage;
