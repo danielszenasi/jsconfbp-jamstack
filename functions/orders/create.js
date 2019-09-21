dad747ca0ab1b6c0463bf47fea7ffb0a85f84b3a
Daniel Szenasi <daniel.szenasi@supercharge.io>
2019-09-21 11:50:14 +0200

    feat: add order function

---
+++ functions/orders/create.js
@@ -0,0 +1,48 @@
+const faunadb = require('faunadb');
+const fetch = require('node-fetch');
+
+/* configure faunaDB Client with our secret */
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET
+});
+
+/* export our lambda function as named "handler" export */
+exports.handler = async (event, context) => {
+  const claims = context.clientContext && context.clientContext.user;
+  /* parse the string body into a useable JS object */
+  const data = JSON.parse(event.body);
+  console.log('Function `create` invoked', data);
+
+  const sendSlackMessage = fetch(process.env.SLACK_WEBHOOK_URL, {
+    headers: {
+      'content-type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({ text: event.body })
+  });
+
+  const item = {
+    data: { ...data, userId: claims.sub }
+  };
+  /* construct the fauna query */
+  const saveOrder = client.query(q.Create(q.Ref('classes/orders'), item));
+
+  return Promise.all([sendSlackMessage, saveOrder])
+    .then(response => {
+      console.log('success', response);
+      /* Success! return the response with statusCode 200 */
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+      };
+    })
+    .catch(error => {
+      console.log('error', error);
+      /* Error! return the error with statusCode 400 */
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      };
+    });
+};
