e2b557f6dc768bbf5dc9fabd3476aeb6e9eaafa8
Daniel Decsi <daniel.decsi@supercharge.io>
2019-09-21 22:02:39 +0200

    refactor: change how cart stores products

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,52 +1,41 @@
-import React from 'react';
-import classNames from 'classnames';
-import { useStore } from '../store/StoreContext';
-import { types } from '../store/types';
-import netlifyIdentity from 'netlify-identity-widget';
+import React from 'react'
+import classNames from 'classnames'
+import { useStore } from '../store/StoreContext'
+import { types } from '../store/types'
+import netlifyIdentity from 'netlify-identity-widget'
 
 function Cart() {
-  const [{ layout, cart }, dispatch] = useStore();
-
-  const products = cart.products.reduce((aggregated, product) => {
-    if (aggregated[product.slug]) {
-      return {
-        ...aggregated,
-        [product.slug]: {
-          ...product,
-          quantity: aggregated[product.slug].quantity + 1,
-          price: aggregated[product.slug].price + product.price
-        }
-      };
-    }
-    return { ...aggregated, [product.slug]: { ...product, quantity: 1 } };
-  }, {});
+  const [{ layout, cart }, dispatch] = useStore()
 
   return (
-    <div className={classNames('cart-container', { 'cart-container-open': layout.isCartOpen })}>
-      {Object.keys(products)
-        .map(k => products[k])
-        .map(product => (
-          <div key={product.slug}>
-            {product.name} - {product.quantity} - {product.price}
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={() =>
-                dispatch({
-                  type: types.DECREASE_QUANTITY,
-                  payload: { slug: product.slug }
-                })
-              }
-            >
-              Remove
-            </button>
-          </div>
-        ))}
+    <div
+      className={classNames('cart-container', {
+        'cart-container-open': layout.isCartOpen,
+      })}
+    >
+      {Object.values(cart.products).map(product => (
+        <div key={product.slug}>
+          {product.name} - {product.quantity} -{' '}
+          {product.price * product.quantity}
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() =>
+              dispatch({
+                type: types.DECREASE_QUANTITY,
+                payload: { slug: product.slug },
+              })
+            }
+          >
+            Remove
+          </button>
+        </div>
+      ))}
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={() =>
           dispatch({
             type: types.CHECKOUT,
-            payload: Object.keys(products).map(k => products[k])
+            payload: cart.products,
           })
         }
       >
@@ -59,7 +48,7 @@ function Cart() {
         Login
       </button>
     </div>
-  );
+  )
 }
 
-export default Cart;
+export default Cart
--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -1,26 +1,48 @@
-import { types } from '../types';
+import { types } from '../types'
 
 export const initialState = {
-  products: []
-};
+  products: {},
+}
 
 export const reducer = (state, action) => {
   switch (action.type) {
-    case types.INCREMENT_QUANTITY:
+    case types.INCREMENT_QUANTITY: {
+      const cartProduct = state.products[action.payload.slug]
+
       return {
         ...state,
-        products: [...state.products, action.payload]
-      };
+        products: {
+          ...state.products,
+          [action.payload.slug]: {
+            ...action.payload,
+            quantity: cartProduct ? cartProduct.quantity + 1 : 1,
+          },
+        },
+      }
+    }
     case types.DECREASE_QUANTITY: {
-      const index = state.products.findIndex(product => product.slug === action.payload.slug);
-      const newProducts = [...state.products];
-      newProducts.splice(index, 1);
       return {
         ...state,
-        products: newProducts
-      };
+        products: Object.keys(state.products).reduce((products, slug) => {
+          if (slug !== action.payload.slug) {
+            return { ...products, [slug]: state.products[slug] }
+          }
+
+          if (state.products[slug].quantity === 1) {
+            return products
+          }
+
+          return {
+            ...products,
+            [slug]: {
+              ...state.products[slug],
+              quantity: state.products[slug].quantity - 1,
+            },
+          }
+        }, {}),
+      }
     }
     default:
-      return state;
+      return state
   }
-};
+}
