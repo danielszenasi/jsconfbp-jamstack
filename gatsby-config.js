d36415ff9c6de6bfb76074186e8bc09a04398cd8
Daniel Decsi <daniel.decsi@supercharge.io>
2019-09-21 19:53:51 +0200

    feat: add gatsby-plugin-netlify-cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,30 +2,31 @@ module.exports = {
   siteMetadata: {
     title: `sourcing markdown recipe`,
     description: `Example sourcing markdown`,
-    author: `@gatsbyjs`
+    author: `@gatsbyjs`,
   },
   plugins: [
     {
       resolve: `gatsby-plugin-layout`,
       options: {
-        component: require.resolve(`./src/components/layout`)
-      }
+        component: require.resolve(`./src/components/layout`),
+      },
     },
     `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `content`,
-        path: `${__dirname}/src/content`
-      }
+        path: `${__dirname}/src/content`,
+      },
     },
     `gatsby-plugin-postcss`,
     {
       resolve: 'gatsby-plugin-purgecss',
       options: {
         tailwind: true,
-        purgeOnly: ['src/css/style.css', 'src/css/global.css']
-      }
-    }
-  ]
-};
+        purgeOnly: ['src/css/style.css', 'src/css/global.css'],
+      },
+    },
+    `gatsby-plugin-netlify-cms`,
+  ],
+}
