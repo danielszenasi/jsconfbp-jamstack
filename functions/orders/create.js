66fbe15ad51ade11745d0f60657bda3efa4e4640
Daniel Szénási <danielszenasi@Daniels-MacBook-Pro.local>
2019-09-22 16:59:17 +0200

    feat: format slack message

--- functions/orders/create.js
+++ functions/orders/create.js
@@ -1,48 +1,54 @@
-const faunadb = require('faunadb');
-const fetch = require('node-fetch');
+const faunadb = require('faunadb')
+const fetch = require('node-fetch')
 
 /* configure faunaDB Client with our secret */
-const q = faunadb.query;
+const q = faunadb.query
 const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SERVER_SECRET
-});
+  secret: process.env.FAUNADB_SERVER_SECRET,
+})
 
 /* export our lambda function as named "handler" export */
 exports.handler = async (event, context) => {
-  const claims = context.clientContext && context.clientContext.user;
+  const claims = context.clientContext && context.clientContext.user
   /* parse the string body into a useable JS object */
-  const data = JSON.parse(event.body);
-  console.log('Function `create` invoked', data);
-
+  const data = JSON.parse(event.body)
+  const message = Object.values(data).map(
+    item => `${item.quantity} ${item.name}`,
+  )
+  console.log(
+    `${claims.email} has succesfully ordered ${message.join(' and ')}`,
+  )
   const sendSlackMessage = fetch(process.env.SLACK_WEBHOOK_URL, {
     headers: {
-      'content-type': 'application/json'
+      'content-type': 'application/json',
     },
     method: 'POST',
-    body: JSON.stringify({ text: event.body })
-  });
+    body: JSON.stringify({
+      text: `${claims.email} has succesfully ordered ${message.join(' and ')}`,
+    }),
+  })
 
   const item = {
-    data: { products: data, userId: claims.sub }
-  };
+    data: { products: data, userId: claims.sub },
+  }
   /* construct the fauna query */
-  const saveOrder = client.query(q.Create(q.Ref('classes/orders'), item));
+  const saveOrder = client.query(q.Create(q.Ref('classes/orders'), item))
 
   return Promise.all([sendSlackMessage, saveOrder])
     .then(response => {
-      console.log('success', response);
+      console.log('success', response)
       /* Success! return the response with statusCode 200 */
       return {
         statusCode: 200,
-        body: JSON.stringify(response)
-      };
+        body: JSON.stringify(response),
+      }
     })
     .catch(error => {
-      console.log('error', error);
+      console.log('error', error)
       /* Error! return the error with statusCode 400 */
       return {
         statusCode: 400,
-        body: JSON.stringify(error)
-      };
-    });
-};
+        body: JSON.stringify(error),
+      }
+    })
+}
